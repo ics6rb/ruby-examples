abda7f454c7d5d545afdf4cd7185aa472462a199
jpyatachkov <cvkucherov@yandex.ru>
2019-11-19 19:59:34 +0300

    Block and Proc example

---
+++ 6/6_3_neibr/test.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'faker'
+require 'minitest/autorun'
+
+require './main.rb'
+require './user.rb'
+
+class TestNeibr < Minitest::Test
+  attr_reader :precision
+
+  def test_raises_argument_error_if_not_numbers_given
+    assert_raises ArgumentError do
+      neibr [[1, 'sd']]
+    end
+  end
+
+  def test_returns_correct_values_if_block_given
+    belongs = [1.0, 0.84]
+    not_belongs = [7.0, 9.0]
+    assert_equal(
+      [[belongs, true], [not_belongs, false]],
+      neibr([belongs, not_belongs], PRECISION) { |x| Math.sin(x**2) }
+    )
+  end
+
+  def test_returns_correct_values_if_proc_given
+    belongs = [1.0, 0.54]
+    not_belongs = [-9.0, 100.0]
+    assert_equal(
+      [[belongs, true], [not_belongs, false]], neibr([belongs, not_belongs], PRECISION, proc { |x| Math.cos(x) })
+    )
+  end
+
+  def test_returns_false_if_no_block_or_proc_given
+    point = [Faker::Number.positive, Faker::Number.positive]
+    assert_equal [[point, false]], neibr([point], PRECISION)
+  end
+end
